64d2482cc5a1bd867e5ee285eae212d137a551c0
tuchin1228 <1104108110@nkust.edu.tw>
2022-09-29 23:18:34 +0800

    review v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {TestTitle} from './StyleComponents/TestStyle.js';
+import MainStyle from './styleModule/main.module.css';
+import styled from 'styled-components';
+import Todolist from './Todolist'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        <h2 className={MainStyle.test}>Style module標題</h2>
+        <Title>StyleComponents標題</Title>
+        <TestTitle>StyleComponents import標題</TestTitle>
+        <h2 className="text-red-500">Tailwind標題</h2>
+
+        <Todolist />
       </header>
     </div>
   );
 }
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: green;
+`;
 
 export default App;
---
+++ src/StyleComponents/TestStyle.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+
+export const TestTitle = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: yellow;
+`;
